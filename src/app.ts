93d4d697dc604ec035853f0a7393ba3a78882271
Dhanuka-12 <danukupe@gmail.com>
2025-09-16 02:20:46 +0530

    implemented routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application } from "express";
+import greetingRoute from "./routes/greeting.route";
 
 const app:Application = express();
 
 app.use(express.json());
-
+app.use("/greeting", greetingRoute)
 app.get("/",(req, res)=>{
     res.send("Hello World");
-});
+});     
 
 const PORT = 3000;
 app.listen(PORT, () => {
