6cf9c12abe85e9527acc22bd7d51d54068f94b8f
Dhanuka-12 <danukupe@gmail.com>
2025-09-16 16:41:54 +0530

    refactor: restructure routing to use singleton pattern for routes

--- src/routes/greeting.route.ts
+++ src/routes/greeting.route.ts
@@ -1,14 +1,27 @@
 import { Router, Request, Response }from "express";
 
-const router = Router();
+export class GreetingRoute{
+    private static instance:GreetingRoute;
+    public static getInstance():GreetingRoute{
+        if(!GreetingRoute.instance){
+            GreetingRoute.instance = new GreetingRoute();
+        }
+        return GreetingRoute.instance;
+    }
+    public router:Router;
+    private constructor(){
+        this.router = Router();
+        this.setupRoutes();
+    }
 
-router.get("/",(req:Request, res:Response) => {
-    res.send("Hello World");
-});
+    private setupRoutes(){
+        this.router.get("/",(req:Request,res:Response) => {
+            res.send("Hello world");
+        });
 
-router.post("/",(req:Request, res:Response) => {
-    const {name} = req.body;
-    res.send(`Hello ${name}`);
-});
-
-export default router;
+        this.router.post("/",(req:Request, res:Response) => {
+            const {name} = req.body;
+            res.send(`Hello ${name}`);
+        });
+    }
+}
